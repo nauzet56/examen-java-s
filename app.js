a35415f9f1d7a019324b1dde1b6e7a903135599d
nauzet56 <betancorpereznauzet@gmail.com>
2026-03-13 12:30:13 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const claveLocalStorage = "paises_juego" //Nombre para local...
+//contadores
+let indice = 0
+let fallos = 0
+let intentos = 0
+
+
+const input = document.getElementById/("emoji");
+const spanAciertos = document.getElementById/("Aciertos")
+const tabla = document.getElementById("historial")
+const btnComprobar = document.getElementById("btnComprobararFormulario")
+const btnReset = document.getElementById("btnResetearFormulario")
+
+
+cargarDatos();
+imagen.src = datos[indice].url
+
+//btnComprobar.addEventListener.
+
+
+
---
+++ datos.js
@@ -0,0 +1,64 @@
+export const items = [
+    {
+        id: 1,
+        country: "España",
+        solution: "🇪🇸",
+        options: ["🇺🇦", "🇪🇸", "🇮🇹"]
+    },
+    {
+        id: 2,
+        country: "Francia",
+        solution: "🇫🇷",
+        options: ["🇮🇹", "🇳🇱", "🇫🇷"]
+    },
+    {
+        id: 3,
+        country: "Italia",
+        solution: "🇮🇹",
+        options: ["🇮🇹", "🇲🇽", "🇭🇺"]
+    },
+    {
+        id: 4,
+        country: "Portugal",
+        solution: "🇵🇹",
+        options: ["🇧🇷", "🇦🇷", "🇵🇹"]
+    },
+    {
+        id: 5,
+        country: "Alemania",
+        solution: "🇩🇪",
+        options: ["🇧🇪", "🇩🇪", "🇦🇹"]
+    },
+    {
+        id: 6,
+        country: "Japón",
+        solution: "🇯🇵",
+        options: ["🇯🇵", "🇨🇳", "🇰🇷"]
+    },
+    {
+        id: 7,
+        country: "Brasil",
+        solution: "🇧🇷",
+        options: ["🇨🇴", "🇧🇴", "🇧🇷"]
+    },
+    {
+        id: 8,
+        country: "Argentina",
+        solution: "🇦🇷",
+        options: ["🇺🇾", "🇦🇷", "🇬🇹"]
+    },
+    {
+        id: 9,
+        country: "México",
+        solution: "🇲🇽",
+        options: ["🇮🇹", "🇲🇽", "🇮🇪"]
+    },
+    {
+        id: 10,
+        country: "Canadá",
+        solution: "🇨🇦",
+        options: ["🇨🇦", "🇺🇸", "🇵🇪"]
+    }
+]
+
+export const localStorageKey = "paises_juego"
